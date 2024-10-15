9ca7d9eaa701386aaf0287726d19c8395c6a06e5
Shweta Singh <shweta.singh@getblueshift.com>
2024-10-15 13:04:46 +0530

    first commit

---
+++ src/shared/sharedStyles.js
@@ -0,0 +1,30 @@
+const sharedStyles = {
+    row: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      marginLeft: '-15px',
+      marginRight: '-15px',
+    },
+    col: {
+      flex: 1,
+      padding: '15px',
+      boxSizing: 'border-box',
+    },
+    colSizes: {
+      col1: { flex: '0 0 8.33%' },
+      col2: { flex: '0 0 16.66%' },
+      col3: { flex: '0 0 25%' },
+      col4: { flex: '0 0 33.33%' },
+      col5: { flex: '0 0 41.66%' },
+      col6: { flex: '0 0 50%' },
+      col7: { flex: '0 0 58.33%' },
+      col8: { flex: '0 0 66.66%' },
+      col9: { flex: '0 0 75%' },
+      col10: { flex: '0 0 83.33%' },
+      col11: { flex: '0 0 91.66%' },
+      col12: { flex: '0 0 100%' },
+    },
+  };
+  
+  export default sharedStyles;
+  
